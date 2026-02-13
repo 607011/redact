67e693708ff0e91a53263333771194ba2c859b50
Oliver Lau <oliver@ersatzworld.net>
2026-02-13 14:48:12 +0100

    Initial commit.

---
+++ redact.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+from typing import Set
+import spacy
+import sys
+import argparse
+
+
+ENTITY_LABELS: Set[str] = {
+    "PERSON", "ORG", "GPE", "LOC", "NUM", "PROPN", "MISC"
+}
+
+nlp = spacy.load("de_core_news_md")
+
+def redact_segment(result: list, start: int, end: int, redact_char: str):
+    for i in range(start, end):
+        if result[i].isalnum():
+            result[i] = redact_char
+
+def redact(text: str, level: int = 70) -> str:
+    doc = nlp(text)
+    # print([f"""{ent.text}: {ent.label_}""" for ent in doc.ents])
+    result = list(text)
+    for chunk in doc.noun_chunks:
+        if level > random.randint(0, 100):
+            redact_segment(result, chunk.start_char, chunk.end_char, "█")
+    return "".join(result)
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, help='Input file path')
+    parser.add_argument('-l', '--level', type=int, default=100, help='Redaction level')
+    args = parser.parse_args()
+    
+    if args.input:
+        with open(args.input, 'r', encoding='utf-8') as f:
+            input_text = f.read()
+    else:
+        input_text = sys.stdin.read()
+    
+    redacted = redact(input_text, args.level)
+    print(redacted)
+
+
+if __name__ == "__main__":
+    main()
